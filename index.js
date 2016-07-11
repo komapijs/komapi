4c9459d16e6318b8b579f95ee2cfecf4ec8ba218
ersims <ersims88@gmail.com>
2016-07-11 16:44:59 +0200

    Add source map support for better stack traces

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // Global init
 global.Promise = require('babel-runtime/core-js/promise').default = require('bluebird');
+require('source-map-support').install();
 
 // Exports
 module.exports = require('./dist/index').default; // eslint-disable-line import/no-unresolved
