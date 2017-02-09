98859ba8d0e04d328d3a601040b5d1a3cf35ca90
ersims <ersims88@gmail.com>
2017-02-09 21:56:58 +0100

    Fix failing test

--- src/lib/findFiles.js
+++ src/lib/findFiles.js
@@ -8,7 +8,7 @@ import recursiveReadDir from 'recursive-readdir-sync';
 export default function findFiles(rootPath, recursive = true) {
   // Create a list of files
   let files = [];
-  if (fileExists(rootPath)) files.push(rootPath);
+  if (fileExists.sync(rootPath)) files.push(rootPath);
   else if (recursive) files = recursiveReadDir(rootPath).filter(p => p.endsWith('.js'));
   else files = fs.readdirSync(rootPath).filter(p => p.endsWith('.js')); // eslint-disable-line security/detect-non-literal-fs-filename
   return files.map(f => path.resolve(f));
